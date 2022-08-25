786886a4688aa78baca2acd46c498bfb6dc09440
Olga Tratsiuk <otratsiuk@gmail.com>
2022-08-25 03:23:08 +0300

    Add some features for SpotifyCrawler

--- SpotifyCrawler.py
+++ SpotifyCrawler.py
@@ -25,32 +25,50 @@ class SpotifyCrawler:
         headers = {"Authorization" : auth}
         response = requests.get(url, headers=headers)
 
-        print('[Response] status_code: ' + str(response.status_code), file=sys.stdout)
-        print('Response headers: ' + str(response.headers), file=sys.stdout)
+        if(response.status_code != 200):
+            print('[Response] status_code: ' + str(response.status_code), file=sys.stdout)
+            print('Response headers: ' + str(response.headers), file=sys.stdout)
 
         if(response.status_code == 429):
             sec_to_sleep = response.headers.get('retry-after')
             print('Retry-after: ' + str(sec_to_sleep))
 
-        return response.json()    
+        return response    
 
 
-    def search(self, search_config):
+    def search_for_artists(self, search_config):
         type = search_config['type']
         year = search_config['year']
         limit = search_config['limit']
 
         url = "https://api.spotify.com/v1/search?q=year%3A" + year + "&type=" + type + "&limit=" + limit + "&offset="
-   
-        for i in range(0, 1000, int(limit)):
-            artist_name = []
+
+        artist_names = []
+        for i in range(0, 100, int(limit)):
             curr_url = url + str(i)
-            print('Curr URL: ' + curr_url)
-            tracks = self.request(curr_url)
-            for i, t in enumerate(tracks['tracks']['items']):
-                artist_name.append(t['artists'][0]['name'])
-        
-            print(artist_name)
+            tracks = self.request(curr_url).json()
+            for i, t in enumerate(tracks['artists']['items']):
+                artist_names.append((t['name'], t['id']))
+
+        return artist_names
+
+
+    def get_artist_tracks(self, artist_name):
+        url = "https://api.spotify.com/v1/artists/" + str(artist_name[1]) + "/top-tracks?market=ES"
+        #url = "https://api.spotify.com/v1/artists/" + str(artist_name[1]) + "/top-tracks"
+        response = self.request(url).json()
+        tracks = []
+        for i, t in enumerate(response['tracks']):
+            tracks.append((t['name'], t['id']))
+    
+        return tracks
+
+
+    def get_track_features(self, track_name):
+        url = "https://api.spotify.com/v1/audio-features/" + str(track_name[1])
+        response = self.request(url).json()
+
+        return response
 
 
 
