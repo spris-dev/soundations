541a091f9544033477111ac5a408b2307ec93bd1
Olga Tratsiuk <otratsiuk@gmail.com>
2022-08-26 02:50:23 +0300

    Update main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from SpotifyCrawler import SpotifyCrawler
 from dotenv import load_dotenv
 load_dotenv()
@@ -9,11 +10,34 @@ def main():
   client_secret = os.environ.get('CLIENT_SECRET')
   spotify_crawler = SpotifyCrawler(client_id, client_secret)                    
 
-  search_config = {'type':'track', 'year':'2000-2020', 'limit':'50'}
+  artists = []
+  for year in range(1920, 2023):
+    search_config = {'type':'artist', 'year':str(year), 'limit':'50'}
+    artists = artists + spotify_crawler.search_for_artists(search_config)
 
-  spotify_crawler.search(search_config)
+  print("NUMBER OF ARTISTS: " + str(len(artists)))
+
+  tracks = []
+  for artist in artists:
+    tracks = tracks + spotify_crawler.get_artist_tracks(artist)
+
+  print("NUMBER OF TRACKS: " + str(len(tracks)))
+
+  track_features = spotify_crawler.get_track_features(tracks[0])
+  columns = track_features.keys()
+
+  dataset = pd.DataFrame(columns=columns)
+
+  track_features = []
+  for track in tracks:
+    track_features.append(spotify_crawler.get_track_features(track))
+
+  track_features = pd.DataFrame(track_features)
+  dataset = pd.concat([dataset, track_features], ignore_index=True)
+  print(dataset)  
+  dataset.to_csv('dataset.csv', index=False)
 
 
 if __name__ == "__main__":
   main()  
-
+  
