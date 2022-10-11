5883b9629287148440f5c57a8abd070260762e4e
Olga Tratsiuk <otratsiuk@gmail.com>
2022-10-11 12:48:27 +0300

    Add some music features

--- SpotifyCrawler.py
+++ SpotifyCrawler.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import time
 
-
+  
 class SpotifyCrawler:
     def __init__(self, client_id, client_secret):
         self.client_id = client_id
@@ -21,33 +21,34 @@ class SpotifyCrawler:
         return response.json()["access_token"]
 
 
-    # def refresh_access_token(self):
-    #     response = requests.post(self.token_url, data={"grant_type": "refresh_token"}, 
-    #                       auth=(self.client_id, self.client_secret))
-
-    #     return response.json()["access_token"]                  
-
-
     def request(self, url):
         auth = "Bearer " + self.token
         headers = {"Authorization" : auth}
         response = requests.get(url, headers=headers)
 
-        if(response.status_code != 200):
+        if response.status_code != 200:
             print('[Response] status_code: ' + str(response.status_code), file=sys.stdout)
             print('Response headers: ' + str(response.headers), file=sys.stdout)
 
-        if(response.status_code == 401):
+        if response.status_code == 401:
             self.token = self.get_access_token()
             print('Token refreshed')
-            self.request(url)
+            return None
 
-        if(response.status_code == 429):
+        if response.status_code == 404:
+            print('404')
+            return None
+
+        if response.status_code == 429:
             sec_to_sleep = response.headers.get('retry-after')
             print('Retry-after: ' + str(sec_to_sleep))
             time.sleep(sec_to_sleep)
+            self.request(url)    
 
-        return response    
+        if response.status_code == 503:
+            self.request(url)
+
+        return response.json() 
 
 
     def search_for_artists(self, search_config):
@@ -60,27 +61,34 @@ class SpotifyCrawler:
         artist_names = []
         for i in range(0, 1000, int(limit)):
             curr_url = url + str(i)
-            tracks = self.request(curr_url).json()
-            for i, t in enumerate(tracks['artists']['items']):
-                artist_names.append((t['name'], t['id']))
+            tracks = self.request(curr_url)
+
+            if tracks is not None:
+                for i, t in enumerate(tracks['artists']['items']):
+                    artist_names.append((t['name'], t['id']))
 
         return artist_names
 
 
     def get_artist_tracks(self, artist_name):
         url = "https://api.spotify.com/v1/artists/" + str(artist_name[1]) + "/top-tracks?market=ES"
-        #url = "https://api.spotify.com/v1/artists/" + str(artist_name[1]) + "/top-tracks"
-        response = self.request(url).json()
-        tracks = []
-        for i, t in enumerate(response['tracks']):
-            tracks.append((t['name'], t['id']))
+        
+        response = self.request(url)
+        if response is not None:
+            tracks = []
+            for i, t in enumerate(response['tracks']):
+                tracks.append([t['name'], t['id'], t['popularity'], t['album']['release_date']])
     
-        return tracks
+            return tracks
 
 
     def get_track_features(self, track_name):
         url = "https://api.spotify.com/v1/audio-features/" + str(track_name[1])
-        response = self.request(url).json()
 
-        return response
-        
+        response = self.request(url)
+        if response is not None:
+            return response
+
+
+    def get_track_release(self, track_name):
+        url = ""        
--- main.py
+++ main.py
@@ -11,27 +11,34 @@ def main():
   spotify_crawler = SpotifyCrawler(client_id, client_secret)                    
 
   artists = []
-  columns = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
+  columns = ['name', 'id', 'popularity', 'release_date', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness',
             'instrumentalness', 'liveness', 'valence', 'tempo', 'type', 'id', 'uri', 'track_href',
             'analysis_url', 'duration_ms', 'time_signature']
 
   dataset = pd.DataFrame(columns=columns)
   dataset.to_csv('dataset.csv', index=False)
 
-  for year in range(1920, 2023):
+  for year in range(1960, 2023):
     search_config = {'type':'artist', 'year':str(year), 'limit':'50'}
     next_artists = spotify_crawler.search_for_artists(search_config)
-    artists = artists + next_artists
+
+    if (next_artists is not None):
+      artists = artists + next_artists
 
     for artist in next_artists:
       track_features = []
       tracks = spotify_crawler.get_artist_tracks(artist)
+      
+      if tracks is not None:
+        for track in tracks:
+          features = spotify_crawler.get_track_features(track)
 
-      for track in tracks:
-        track_features.append(spotify_crawler.get_track_features(track))
+          if features is not None:
+            analys = spotify_crawler.get_track_features(track)
+            track_features.append([*track, *analys.values()])
 
-      sub_dataset = pd.DataFrame(track_features)      
-      sub_dataset.to_csv('dataset.csv', index=False, mode="a", header=False) 
+        sub_dataset = pd.DataFrame(track_features)      
+        sub_dataset.to_csv('dataset.csv', index=False, mode="a", header=False) 
 
 
 if __name__ == "__main__":
